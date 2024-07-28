514776a6d9d7441a6ced574786bf9160003c1a4f
ayush <ayush.borhade2004@gmail.com>
2024-07-28 17:31:54 +0530

    added cars app and fetched the data into home page

--- pages/models.py
+++ pages/models.py
@@ -6,7 +6,7 @@ class Team(models.Model):
     last_name = models.CharField(max_length=255)
     designation = models.CharField(max_length=255)
     photo = models.ImageField(upload_to = 'photos/%Y/%m/%d')
-    facebok_link = models.URLField(max_length=100)
+    facebook_link = models.URLField(max_length=100)
     twitter_link = models.URLField(max_length=100)
     google_plus_link = models.URLField(max_length=100)
     creted_date = models.DateTimeField(auto_now_add= True)
